18ab7ce26b91809b6cf901d26c1456ec641dcabf
Keefer Davies <keeferdavies@rogers.com>
2015-02-21 16:04:33 -0500

    Reverted flatbuffers to earlier reviesion. Started to integrate externals build.

---
+++ src/qmake/linux_gmake.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+runCommand()
+{
+	echo $1 && 	$1
+	if [ "$?" != "0" ]; then
+		echo "ERROR : $1" && exit 1
+	fi
+}
+
+echo "--Building PGTA Composer Dependencies--"
+
+runCommand './../external/build-tools/bin/premake/premake5_linux clean --file=externals.lua'
+runCommand './../external/build-tools/bin/premake/premake5_linux gmake --file=externals.lua'
+
+pushd gmake/
+
+runCommand 'make clean'
+runCommand 'make'
+
+popd
+
+echo "--Building PGTA Composer Applicatoin--"
+
+runCommand 'qmake appsuite.pro'
+runCommand 'make clean'
+runCommand 'make'
