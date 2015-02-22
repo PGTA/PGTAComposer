525e9c2d4847853f13c4d861d164e9dd836bccb5
Keefer Davies <keeferdavies@rogers.com>
2015-02-21 21:39:15 -0500

    Windows build script cleanup.

--- src/qmake/vs2013.bat
+++ src/qmake/vs2013.bat
@@ -1,10 +1,10 @@
 echo "--Setting up Build Environment--"
 
-set msbuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
-set qmake=C:\Qt\5.3\msvc2013\bin\qmake.exe
-set jom=C:\Qt\Tools\QtCreator\bin\jom.exe
+set msbuild="%VS120COMNTOOLS%\..\..\..\MSBuild\12.0\Bin\MSBuild"
+set qmake="C:\Qt\5.3\msvc2013\bin\qmake"
+set jom="C:\Qt\Tools\QtCreator\bin\jom"
 
-call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 
 echo "--Building PGTA Composer Dependencies--"
 
@@ -13,14 +13,14 @@ echo "--Building PGTA Composer Dependencies--"
 
 pushd vs2013
 
-%msbuild% /nologo /p:Configuration=Debug /p:Platform=Win32 /p:VisualStudioVersion=13.0 Externals.sln
+call %msbuild% /nologo /p:Configuration=Release /p:Platform=Win32 /p:VisualStudioVersion=13.0 Externals.sln
 
 popd
 
-echo "--Building PGTA Composer Applicatoin--"
+echo "--Building PGTA Composer Application--"
 
-%qmake% appsuite.pro
+call %qmake% appsuite.pro
 
-%jom%
+call %jom%
 
 pause
