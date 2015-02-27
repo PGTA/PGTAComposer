8bbac83b0c316821efe19a6693bc3f7755e27ac1
Keefer Davies <keeferdavies@rogers.com>
2015-02-26 21:53:53 -0500

    Work on composer build scripts.

--- src/qmake/linux_gmake.sh
+++ src/qmake/linux_gmake.sh
@@ -2,30 +2,40 @@
 
 runCommand()
 {
-    echo $1 && $1
+    echo $1 && eval $1
     if [ "$?" != "0" ]; then
         echo "ERROR : $1" && exit 1
     fi
 }
 
-export BIN="./../bin/"
-export GMAKE="./gmake/"
-export PGTA_PREMAKE="./../external/PGTA/src/premake/"
+# capture current working directory
+export QMAKEDIR=$(pwd)
+
+# setup build directories
+export BIN="$QMAKEDIR/../bin/"
+export BUILD="$QMAKEDIR/gmake/"
+
+# dependencies
+export PGTA_PREMAKE="$QMAKEDIR/../external/PGTA/src/premake/"
+
+# make gmake directory if it doesn't exist
+test -d $BUILD || runCommand "mkdir -p $BUILD"
 
 echo "--Building PGTA Composer Dependencies--"
 
 pushd $PGTA_PREMAKE
+    runCommand "./linux_gmake.sh"
+popd
 
-runCommand "./linux_gmake.sh"
+echo "--Creating Symbolic Links--"
 
+pushd $build
+    test -L lib_FlatBuffers.a || runCommand "ln -s $PGTA_PREMAKE/gmake/lib_FlatBuffers.a lib_FlatBuffers.a"
 popd
 
 echo "--Copying PGTA Binaries--"
 
-runCommand "mkdir -p $GMAKE"
-runCommand "cp -r $PGTA_PREMAKE/gmake/* $GMAKE"
-runCommand "mkdir -p $BIN"
-runCommand "cp -r $PGTA_PREMAKE/../../bin/* $BIN"
+# TODO : Copy library used to play/pause/stop PGTA track
 
 echo "--Building PGTA Composer Application--"
 
--- src/qmake/osx_gmake.sh
+++ src/qmake/osx_gmake.sh
@@ -2,30 +2,40 @@
 
 runCommand()
 {
-    echo $1 && $1
+    echo $1 && eval $1
     if [ "$?" != "0" ]; then
         echo "ERROR : $1" && exit 1
     fi
 }
 
-bin="./../bin/"
-gmake="./gmake/"
-pgta_premake="./../external/PGTA/src/premake/"
+# capture current working directory
+qmakedir=$(pwd)
+
+# setup build directories
+bin="$qmakedir/../bin/"
+build="$qmakedir/gmake/"
+
+# dependencies
+pgta_premake="$qmakedir/../external/PGTA/src/premake/"
+
+# make gmake directory if it doesn't exist
+test -d $build || runCommand "mkdir -p $build"
 
 echo "--Building PGTA Composer Dependencies--"
 
 pushd $pgta_premake
+    runCommand "./osx_gmake.sh"
+popd
 
-runCommand "./osx_gmake.sh"
+echo "--Creating Symbolic Links--"
 
+pushd $build
+    test -L lib_FlatBuffers.a || runCommand "ln -s $pgta_premake/gmake/lib_FlatBuffers.a lib_FlatBuffers.a"
 popd
 
 echo "--Copying PGTA Binaries--"
 
-runCommand "mkdir -p $gmake"
-runCommand "cp -r $pgta_premake/gmake/* $gmake"
-runCommand "mkdir -p $bin"
-runCommand "cp -r $pgta_premake/../../bin/* $bin"
+# TODO : Copy library used to play/pause/stop PGTA track
 
 echo "--Building PGTA Composer Application--"
 
