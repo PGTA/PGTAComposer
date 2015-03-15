ce9096b1192c36db17981a0cde725e4eb579b022
Keefer Davies <keeferdavies@rogers.com>
2015-03-15 03:57:03 -0400

    Added formatting for log messages.

--- src/pgta_composer/main.cpp
+++ src/pgta_composer/main.cpp
@@ -2,10 +2,38 @@
 #include <QDesktopWidget>
 #include <QFile>
 #include <QDir>
+#include <QTime>
+#include <string>
 #include "mainwindow.h"
+
+// use qDebug(), qWarning(), qCritical(), qFatal() to log
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    std::string currentTime = QTime::currentTime().toString("hh:mm:ss.zzz").toStdString();
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "%s [DEBUG]: %s (%s:%u)\n", currentTime.c_str(), localMsg.constData(),
+                context.file, context.line);
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "%s [WARNING]: %s (%s:%u)\n", currentTime.c_str(), localMsg.constData(),
+                context.file, context.line);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "%s [CRITICAL]: %s (%s:%u)\n", currentTime.c_str(), localMsg.constData(),
+                context.file, context.line);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "%s [FATAL]: %s (%s:%u)\n", currentTime.c_str(), localMsg.constData(),
+                context.file, context.line);
+        abort();
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput);
     QApplication mainApp(argc, argv);
     MainWindow mainWindow;
 
